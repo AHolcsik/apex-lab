f40f07df3629d73861aac565c494cb04331310e2
A Holcsik <you@example.com>
2024-09-20 17:15:41 +0200

    established websocket connection; clean up

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import Monitor from "./components/Monitor";
 import Alerts from "./components/Alerts";
 import Navigation from "./components/Navigation";
 import "bootstrap/dist/css/bootstrap.min.css";
+import useSocket from "./components/useSocket";
 
 function App() {
+  useSocket();
   return (
     <>
       <Navigation />
